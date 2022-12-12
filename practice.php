bb9953b26e5df5a35b9ad33a93dddfa98c60a7a0
hasebekazuya <tabi85nack5877@icloud.com>
2022-12-12 06:32:22 +0000

    PHP/Laravel03課題完了

--- practice.php
+++ practice.php
@@ -1,31 +1,26 @@
 <?php
 echo 'hello php!';
-?>
 
-<?php
+
 $a = 3;
 $b = 7;
 echo $a + $b . PHP_EOL;
-?>
 
-<?php
+
 $array_month = ['1月', '2月', '3月',  '4月',  '5月',  '6月',  '7月',  '8月',  '9月',  '10月',  '11月',  '12月'];
 echo $array_month[7];
-?>
 
-<?php
+
 $hello = "Hello,";
 $name = "長谷部";
 echo $hello . $name . PHP_EOL;
-?>
 
-<?php
+
 $tech_boost ="tech";
 $tech_boost .=" boost";
 echo $tech_boost . PHP_EOL;
-?>
 
-<?php
+
 $calendar =[
     "January" => "1月",
     "February" => "2月",
@@ -41,4 +36,29 @@ $calendar =[
     "December" => "12月"
 ];
 echo $calendar["December"] . PHP_EOL;
-?>
+
+$name = "kazuya" ;
+if ($name == "kazuya") {
+   echo "私は ".$name."です".PHP_EOL;
+}else{
+    echo $name."ではありません。" . PHP_EOL;
+}
+
+$total = 0;
+for ($i =0; $i <= 10000; $i ++) {
+$total + $i;
+}
+echo $total . PHP_EOL;
+
+$fruits = ["strawberry", "apple", "Japnese apple", "pine apple", "grape"] ;
+foreach ($fruits as $fruit) {
+    echo $fruit.PHP_EOL;
+}
+
+$start = 1;
+$end = 100;
+for ($i = $start; $i <= $end; $i ++) {
+    if ($i %  5 == 0) {
+        echo $i;
+    }
+}
